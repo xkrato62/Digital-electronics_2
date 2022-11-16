ae23cf1e971262e881679c8b55e06e5c5e150610
Kratochvíl Dušan (230276) <xkrato62@vutbr.cz>
2022-11-16 12:44:41 +0100

    lab08

---
+++ Labs/Lab08/lab8-asm/documention/html/search/all_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['lab_208_3a_20insert_5fyour_5ffirstname_20insert_5fyour_5flastname_0',['Lab 8: INSERT_YOUR_FIRSTNAME INSERT_YOUR_LASTNAME',['../md_test__r_e_a_d_m_e.html',1,'']]],
+  ['lab08_2dasm_1',['lab08-asm',['../index.html',1,'']]],
+  ['lfsr4_5ffibonacci_5fasm_2',['lfsr4_fibonacci_asm',['../group__fryza__asm.html#ga73300f0d004c2b00b877e2285c99b1f2',1,'main.c']]]
+];
---
+++ Labs/Lab08/lab8-asm/documention/html/search/pages_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['lab_208_3a_20insert_5fyour_5ffirstname_20insert_5fyour_5flastname_0',['Lab 8: INSERT_YOUR_FIRSTNAME INSERT_YOUR_LASTNAME',['../md_test__r_e_a_d_m_e.html',1,'']]],
+  ['lab08_2dasm_1',['lab08-asm',['../index.html',1,'']]]
+];
